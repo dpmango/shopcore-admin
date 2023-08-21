116bf9ee2473c2158b89a496d69987a0c3872208
Sergey Khmelevskoy <xs290@me.com>
2023-08-21 20:01:11 +0300

    up

--- src/core/hooks/useTheme.ts
+++ src/core/hooks/useTheme.ts
@@ -6,11 +6,11 @@ export const useTheme = () => {
     const $html = document.querySelector('html')
     $html?.classList.toggle('light')
 
-    if (localStorageGet('theme') === 'light') {
-      localStorageSet('theme', 'dark')
+    if (localStorageGet('theme-admin') === 'light') {
+      localStorageSet('theme-admin', 'dark')
       dispatch(setTheme('dark'))
     } else {
-      localStorageSet('theme', 'light')
+      localStorageSet('theme-admin', 'light')
       dispatch(setTheme('light'))
     }
 
@@ -20,7 +20,7 @@ export const useTheme = () => {
   const setInitialTheme = useCallback(() => {
     const $html = document.querySelector('html')
     let isDark = false
-    if (localStorageGet('theme') === 'dark') {
+    if (localStorageGet('theme-admin') === 'dark') {
       $html?.classList.remove('light')
       isDark = true
     } else {
